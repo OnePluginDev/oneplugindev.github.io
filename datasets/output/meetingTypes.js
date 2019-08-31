0afe0ddd05b7742f2fa63eb6c40dd7d23ea7a932
= <nathanreddycode@gmail.com>
2019-08-31 15:16:53 -0500

    database seeding

---
+++ datasets/output/meetingTypes.js
@@ -0,0 +1,54 @@
+var data = {
+    LEC: { meetingType: 'LEC',
+      meetingScheduleType: 'LEC',
+      meetingTypeDescription: 'Lecture'
+    },
+   EXAM: { meetingType: 'EXAM',
+      meetingScheduleType: 'LEC',
+      meetingTypeDescription: 'Examination'
+    },
+   LAB: { meetingType: 'LAB',
+      meetingScheduleType: 'LL',
+      meetingTypeDescription: 'Laboratory'
+    },
+   SEM: { meetingType: 'SEM',
+      meetingScheduleType: 'LEC',
+      meetingTypeDescription: 'Seminar'
+    },
+   PRAC: { meetingType: 'PRAC',
+      meetingScheduleType: 'PRA',
+      meetingTypeDescription: 'Practicum'
+    },
+   REC: { meetingType: 'REC',
+      meetingScheduleType: 'LEC',
+      meetingTypeDescription: 'Recitation'
+    },
+   INS: { meetingType: 'INS',
+      meetingScheduleType: 'INS',
+      meetingTypeDescription: 'Independent Study'
+    },
+   PRL: { meetingType: 'PRL',
+      meetingScheduleType: 'PRL',
+      meetingTypeDescription: 'Private Lesson'
+    },
+   RES: { meetingType: 'RES',
+      meetingScheduleType: 'RES',
+      meetingTypeDescription: 'Research'
+    },
+   CLD: { meetingType: 'CLD',
+      meetingScheduleType: 'CLD',
+      meetingTypeDescription: 'Clinic'
+    },
+   CMP: { meetingType: 'CMP',
+      meetingScheduleType: 'CMP',
+      meetingTypeDescription: 'Competition'
+    },
+   CLAS: { meetingType: 'CLAS',
+      meetingScheduleType: 'LEC',
+      meetingTypeDescription: 'Class'
+    },
+   INT: { meetingType: 'INT',
+      meetingScheduleType: 'PRA',
+      meetingTypeDescription: 'Internship'
+    }
+}
